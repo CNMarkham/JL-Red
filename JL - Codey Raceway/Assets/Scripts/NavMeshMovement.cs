fdde77e90360d0274f52be0836b5178a79a4ef0f
DAMINA MINA EH EHHH <96213580+ilovetapwaterr@users.noreply.github.com>
2023-09-10 15:59:01 -0400

    NavMeshClosestObject

--- JL - Codey Raceway/Assets/Scripts/NavMeshMovement.cs
+++ JL - Codey Raceway/Assets/Scripts/NavMeshMovement.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NavMeshMovement : MonoBehaviour
 {
+    public GameObject [] obstacles;
+    public Transform goal;
+    private NavMeshAgent agent;
+    public Vector3 avatarPosition;
+    public Transform avatar;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        agent = GetComponent<NavMeshAgent>();
+        obstacles = GameObject.FindGameObjectsWithTag("obstacle");
+        avatarPosition = gameObject.transform.position;
 
+        for (int i = 0; i <= obstacles.Length; i++)
+        {
+            Vector3.Distance(avatarPosition, obstacles[i].transform.position);
+        }
+
+        agent.destination = goal.position;
+    }
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "obstacle")
+        {
+            gameObject.SetActive(false);
+            collision.gameObject.SetActive(false);
+        }
     }
 }
